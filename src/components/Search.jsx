906772dc2e69cc78607eef5b58d1b73a4e96cf66
Sunny <gsaipraneeth25@gmail.com>
2024-09-18 20:46:01 -0400

    add useEffect to fetch data via API in search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,13 +1,24 @@
 import { useState } from "react";
+import { useEffect } from "react";
 
-export function Search() {
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "711bf94f13354450aee7ae592a8c367e";
+export default function Search() {
   const [query, setQuery] = useState("pizza");
+  useEffect(() => {
+    async function fetchFood() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+    }
+    fetchFood();
+  }, [query]);
   return (
     <div>
       <input
-        type="text"
         value={query}
         onChange={(e) => setQuery(e.target.value)}
+        type="text"
       />
     </div>
   );
